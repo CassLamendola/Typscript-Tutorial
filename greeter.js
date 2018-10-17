f279bc471c995ce25c8d8f62632921a396a0ff5c
Cassandra Lamendola <cass.lamendola@gmail.com>
2018-10-16 21:40:10 -0700

    implement factory pattern

--- greeter.js
+++ greeter.js
@@ -1,14 +1,99 @@
-var Student = /** @class */ (function () {
-    function Student(firstName, middleInitial, lastName) {
-        this.firstName = firstName;
-        this.middleInitial = middleInitial;
-        this.lastName = lastName;
-        this.fullName = firstName + ' ' + middleInitial + ' ' + lastName;
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
     }
-    return Student;
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var Person = /** @class */ (function () {
+    function Person(firstName, middleInitial, lastName, age) {
+        this.fullName = firstName + ' ' + middleInitial + ' ' + lastName;
+        this.age = age;
+    }
+    Person.prototype.greet = function () {
+        return "Hello " + this.fullName;
+    };
+    Person.prototype.giveAge = function () {
+        return "I am " + this.age + " years old";
+    };
+    return Person;
 }());
-function greeter(person) {
-    return 'Hello, ' + person.firstName + '' + person.lastName;
+var Student = /** @class */ (function (_super) {
+    __extends(Student, _super);
+    function Student(firstName, middleInitial, lastName, age, courses) {
+        var _this = _super.call(this, firstName, middleInitial, lastName, age) || this;
+        _this.courses = courses;
+        return _this;
+    }
+    Student.prototype.listCourses = function () {
+        return "Courses: " + this.courses.join(', ');
+    };
+    return Student;
+}(Person));
+var Teacher = /** @class */ (function (_super) {
+    __extends(Teacher, _super);
+    function Teacher(firstName, middleInitial, lastName, age, students) {
+        var _this = _super.call(this, firstName, middleInitial, lastName, age) || this;
+        _this.students = students;
+        return _this;
+    }
+    Teacher.prototype.listStudents = function () {
+        return "Students: " + this.students.join(', ');
+    };
+    return Teacher;
+}(Person));
+var PersonCreator = /** @class */ (function () {
+    function PersonCreator() {
+    }
+    PersonCreator.prototype.createPerson = function (options) {
+        if (options.type === 'student') {
+            return StudentCreator(options.firstName, options.middleInitial, options.lastName, options.age, options.courses);
+        }
+        else if (options.type === 'teacher') {
+            return TeacherCreator(options.firstName, options.middleInitial, options.lastName, options.age, options.courses);
+        }
+        else {
+            throw new Error('Select either student or teacher');
+        }
+    };
+    return PersonCreator;
+}());
+function StudentCreator(firstName, middleInitial, lastName, age, courses) {
+    return new Student(firstName, middleInitial, lastName, age, courses);
 }
-var user = new Student('Jane', 'M.', 'User');
-document.body.innerHTML = greeter(user);
+function TeacherCreator(firstName, middleInitial, lastName, age, students) {
+    return new Teacher(firstName, middleInitial, lastName, age, students);
+}
+function greeter(person) {
+    return person.greet();
+}
+var personCreator = new PersonCreator();
+var courses = ['Algebra', 'Algorithms', 'Biology', 'Rocket Science'];
+var jane = personCreator.createPerson({
+    firstName: 'Jane',
+    middleInitial: 'M.',
+    lastName: 'Doe',
+    age: 25,
+    courses: courses,
+    type: 'student'
+});
+document.getElementById('1').innerHTML = jane.listCourses();
+document.getElementById('2').innerHTML = jane.giveAge();
+var students = ['Jane', 'Bobby', 'Joe'];
+var sally = personCreator.createPerson({
+    firstName: 'Sally',
+    middleInitial: 'L.',
+    lastName: 'Johnson',
+    age: 39,
+    courses: students,
+    type: 'teacher'
+});
+document.getElementById('3').innerHTML = sally.listStudents();
+document.getElementById('4').innerHTML = sally.giveAge();
+document.getElementById('5').innerHTML = greeter(jane);
--- greeter.ts
+++ greeter.ts
@@ -1,19 +1,98 @@
-class Student {
-  fullName: string;
-  constructor(public firstName: string, public middleInitial: string, public lastName: string) {
-    this.fullName = firstName + ' ' + middleInitial + ' ' + lastName;
-  }
+class Person {
+	private fullName: string;
+	private age: number;
+	constructor(firstName: string, middleInitial: string, lastName: string, age: number) {
+		this.fullName = firstName + ' ' + middleInitial + ' ' + lastName;
+		this.age = age;
+	}
+
+	greet() {
+		return `Hello ${this.fullName}`;
+	}
+
+    giveAge() {
+		return `I am ${this.age} years old`;
+    }
 }
 
-interface Person {
-  firstName: string;
-  lastName: string;
+class Student extends Person {
+  private courses;
+	constructor(firstName, middleInitial, lastName, age, courses) {
+		super(firstName, middleInitial, lastName, age);
+	  this.courses = courses;
+	}
+
+    listCourses() {
+		return `Courses: ${this.courses.join(', ')}`;
+	}
+}
+
+class Teacher extends Person {
+  private students;
+    constructor(firstName, middleInitial, lastName, age, students) {
+		super(firstName, middleInitial, lastName, age);
+		this.students = students;
+	}
+
+    listStudents() {
+		return `Students: ${this.students.join(', ')}`;
+    }
+}
+
+class PersonCreator {
+	createPerson(type: Object);
+	createPerson(type: 'student'): Student;
+	createPerson(type: 'teacher'): Teacher;
+
+    public createPerson(options): Student | Teacher {
+        if (options.type === 'student') {
+			return StudentCreator(options.firstName, options.middleInitial, options.lastName, options.age, options.courses)
+        } else if (options.type === 'teacher') {
+			return TeacherCreator(options.firstName, options.middleInitial, options.lastName, options.age, options.courses)
+        } else {
+			throw new Error('Select either student or teacher')
+        }
+    }
+}
+
+function StudentCreator(firstName: string, middleInitial: string, lastName: string, age: number, courses) {
+    return new Student(firstName, middleInitial, lastName, age, courses);
+}
+
+function TeacherCreator(firstName: string, middleInitial: string, lastName: string, age: number, students) {
+    return new Teacher(firstName, middleInitial, lastName, age, students);
 }
 
 function greeter(person: Person) {
-  return 'Hello, ' + person.firstName + '' + person.lastName;
+  	return person.greet()
 }
 
-let user = new Student('Jane', 'M.', 'User');
+const personCreator = new PersonCreator()
 
-document.body.innerHTML = greeter(user);
+let courses = ['Algebra', 'Algorithms', 'Biology', 'Rocket Science'];
+let jane = personCreator.createPerson({
+  firstName: 'Jane', 
+  middleInitial: 'M.', 
+  lastName: 'Doe',
+  age: 25,
+  courses: courses, 
+  type: 'student'
+});
+
+document.getElementById('1').innerHTML = jane.listCourses();
+document.getElementById('2').innerHTML = jane.giveAge();
+
+let students = ['Jane', 'Bobby', 'Joe']
+let sally = personCreator.createPerson({
+  firstName: 'Sally', 
+  middleInitial: 'L.', 
+  lastName: 'Johnson',
+  age: 39,
+  courses: students, 
+  type: 'teacher'
+});
+
+document.getElementById('3').innerHTML = sally.listStudents();
+document.getElementById('4').innerHTML = sally.giveAge();
+
+document.getElementById('5').innerHTML = greeter(jane);
